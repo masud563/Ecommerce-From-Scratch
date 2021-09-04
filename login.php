ef6106178686d9837f87ce5f554ad3a3876b1fbe
masud563 <sydmasud563@gmail.com>
2021-09-04 21:54:17 +0600

    A huge update

--- login.php
+++ login.php
@@ -1,43 +1,33 @@
-<?php include('server.php');?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <h2>Login</h2>
-        </div>
+<!--
+Into this file, we create a layout for login page.
+-->
+
+<?php
+include_once('header.php');
+include_once('link.php');
+
+?>
+
+<div id="frmRegistration">
+<form class="form-horizontal" method="POST" action="login_code.php">
+	<h1>User Login</h1>
+	
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-6">
+      <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
     </div>
-    <form action="login.php" method="post">
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" required>
-        </div>
-
-        
-
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password_1" required>
-        </div>
-
-        
-
-        <button type="submit" name="login_user">Log in</button>
-
-        <P>not a User?<a href="registration.php"><b>Register Here</b></P>
-
-
-
-
-    </form>
-
-
-    
-</body>
-</html>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <div class="col-sm-6"> 
+      <input type="password" class="form-control" name="password" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" name="login" class="btn btn-primary">Login</button>
+    </div>
+  </div>
+</form>
+</div>
--- registration.php
+++ registration.php
@@ -1,53 +1,49 @@
-<?php include('server.php'); ?>
+<!--
+Into this file, we create a layout for registration page.
+-->
+<?php
+include_once('header.php');
+include_once('link.php');
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>registration</title>
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <h2>register</h2>
-        </div>
+<div id="frmRegistration">
+<form class="form-horizontal" action="registration_code.php" method="POST">
+	<h1>User Registration</h1>
+	<div class="form-group">
+    <label class="control-label col-sm-2" for="firstname">First Name:</label>
+    <div class="col-sm-6">
+      <input type="text" name="firstname" class="form-control" id="firstname" placeholder="Enter Firstname">
     </div>
-    <form action="registration.php" method="post">
-        <?php include('errors.php'); ?>
-
-
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name='username' required>
-        </div>
-
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" required>
-        </div>
-
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password_1" required>
-        </div>
-
-        <div>
-            <label for="password">Confirm Password</label>
-            <input type="password" name="password_2" required>
-        </div>
-
-        <button type="submit" name="reg_user">Submit</button>
-
-        <P>already a User?<a href="login.php"><b>Log in</b></P>
-
-
-
-
-    </form>
-
-
-    
-</body>
-</html>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="lastname">Last Name:</label>
+    <div class="col-sm-6">
+      <input type="text" name="lastname" class="form-control" id="lastname" placeholder="Enter Lastname">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="lastname">Gender:</label>
+    <div class="col-sm-6">
+      <label class="radio-inline"><input type="radio" name="gender" value="Male">Male</label>
+	  <label class="radio-inline"><input type="radio" name="gender" value="Female">Female</label>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="email">Email:</label>
+    <div class="col-sm-6">
+      <input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="pwd">Password:</label>
+    <div class="col-sm-6"> 
+      <input type="password" name="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" name="create" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+</div>
