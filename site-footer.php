ec9233561d8f22cacc39b905808bd6aa1691ad67
masud563 <sydmasud563@gmail.com>
2021-09-13 16:34:01 +0600

    site-header and site-footer are included

---
+++ site-footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Brand Mart- Shop with joy.</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yesteryear&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="index-responsive.css">
+</head>
+<body>
+    <footer class="footer">
+        <div class="buy">
+            <div class="buy-buy">
+                <a href="#">Buy</a>
+            </div>
+            <div class="buy-registration">
+                <a href="#">Registration</a>
+            </div>
+            <div class="buy-store">
+                <a href="#">Store</a>
+            </div>
+            <div class="buy-shopping-guide">
+                <a href="#">Shopping Guide</a>
+            </div>
+        </div>
+        <div class="sell">
+            <div class="sell-start">
+                <a href="#">Start Selling</a>
+            </div>
+            <div class="sell-learn-to-sell">
+                <a href="#">Learn to sell</a>
+            </div>
+            
+        </div>
+        
+        <div class="about">
+            <div class="about-about">
+                <a href="#">About The brand Mart</a>
+            </div>
+            <div class="about-company-info">
+                <a href="#">Company info</a>
+            </div>          
+        </div>
+        <div class="help"></div>
+        <div class="newsletter"></div>        
+    </footer>
+</body>
+</html>
